5660ad65b45226621eee0b4054f2d25ab975a717
chaldiran527 <90536610+chaldiran527@users.noreply.github.com>
2024-06-17 07:50:06 -0600

    Update actualizarActividades.js

--- actualizarActividades.js
+++ actualizarActividades.js
@@ -17,7 +17,7 @@ const enviarNotificacion = async (actividad, tipo) => {
 
 const setFechaSimulada = async () => {
     try {
-        const response = await fetch('https://sd-project2-frontend.vercel.app/api/fechaSistemaRoutes/getFechaSimulada', {
+        const response = await fetch('https://sdproject2-api.onrender.com/api/fechaSistemaRoutes/getFechaSimulada', {
             method: 'GET',
             headers: {
                 'Content-Type': 'application/json'
