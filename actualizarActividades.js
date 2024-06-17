69960973207d7ce2b35d9bee2f5d8d8033b7713a
Chaldiran527 <Panipatsurvivor@gmail.com>
2024-06-17 03:47:15 -0600

    Upload final version

--- actualizarActividades.js
+++ actualizarActividades.js
@@ -1,66 +1,93 @@
-// Importa el modelo de Actividad aquí
-const Actividad = require('./models/actividadModel'); // Asegúrate de usar la ruta correcta según tu estructura de archivos
-const Notificacion = require('./models/notificacionModel'); // Asegúrate de usar la ruta correcta según tu estructura de archivos
-
+const Actividad = require('./models/actividadModel');
+const Notificacion = require('./models/notificacionModel'); 
+//const { PublishVisitor, ReminderVisitor } = require('./visitor');
+//const { NotificationCenter } = require('./Observer'); // Importamos NotificationCenter
 let fechaSimulada;
 
 const enviarNotificacion = async (actividad, tipo) => {
-    // Implementar la lógica de envío de notificación
     const nuevaNotificacion = new Notificacion({
         contenido: `${tipo}: ${actividad.nombre}`,
         emisor: 'Sistema',
-        receptor: actividad.personasResponsables,
         fechaHora: new Date(),
-        estado: 'no leído'
+        estado: 'no leído',
+        estudiantesInscritos: actividad.estudiantesInscritos
     });
     await nuevaNotificacion.save();
 };
 
 const setFechaSimulada = async () => {
     try {
-      const response = await fetch('http://localhost:3000/api/fechaSistemaRoutes/getFechaSimulada', {
-        method: 'GET',
-        headers: {
-          'Content-Type': 'application/json'
-        },
-      });
-      if (!response.ok) {
-        throw new Error('Error al obtener la fecha simulada');
-      }
-      const data = await response.json();
-      fechaSimulada = new Date(data.fechaSimulada);
-      console.log('Fecha simulada actualizada:', fechaSimulada);
+        const response = await fetch('http://localhost:3000/api/fechaSistemaRoutes/getFechaSimulada', {
+            method: 'GET',
+            headers: {
+                'Content-Type': 'application/json'
+            },
+        });
+        if (!response.ok) {
+            throw new Error('Error al obtener la fecha simulada');
+        }
+        const data = await response.json();
+        fechaSimulada = new Date(data.fechaSimulada);
+        console.log('Fecha simulada actualizada:', fechaSimulada);
     } catch (error) {
-      console.error('Error al obtener fecha simulada:', error);
+        console.error('Error al obtener fecha simulada:', error);
     }
-  };
+};
+
+
+//const notificationCenter = new NotificationCenter();
+//class NotificacionObserver extends Observer {
+    //update(actividad, tipo) {
+        //enviarNotificacion(actividad, tipo); // Llama a la función de enviarNotificacion
+    //}
+//}
+
+
+//const notificacionObserver = new NotificacionObserver();
+
+
+//notificationCenter.attach(notificacionObserver);
+
+
 
 const actualizarEstadoActividades = async () => {
-  try {
-    console.log("a")
-    const actividades = await Actividad.find();
-    const ahora = fechaSimulada || new Date();
+    try {
+        const actividades = await Actividad.find();
+        const ahora = fechaSimulada || new Date();
 
-    for (let actividad of actividades) {
-      const { fechaPublicacion, fechasRecordatorio, estadoActividad } = actividad;
-      if (estadoActividad === 'Planeada' && ahora >= fechaPublicacion) {
-        
-        actividad.estadoActividad = 'Notificada';
-        await actividad.save();
-        await enviarNotificacion(actividad, 'Actividad Publicada');
-      }
+        //const publishVisitor = new PublishVisitor();
+        //const reminderVisitor = new ReminderVisitor();
 
-      for (let fechaRecordatorio of fechasRecordatorio) {
-        console.log(fechaRecordatorio)
-        if (estadoActividad === 'Notificada' && ahora >= fechaRecordatorio) {
-          console.log("recorda")
-          await enviarNotificacion(actividad, 'Recordatorio de Actividad');
+        for (let actividad of actividades) {
+            const { fechaPublicacion, fechasRecordatorio, estadoActividad, notificado, recordatoriosEnviados } = actividad;
+
+            // VERIFICAR PARA PUBLICAR
+            if (estadoActividad === 'Planeada' && ahora >= fechaPublicacion && !notificado) {
+                actividad.estadoActividad = 'Notificada';
+                actividad.notificado = true;
+                await actividad.save();
+                await enviarNotificacion(actividad, 'Actividad Publicada');
+                //NotificationCenter.notify(actividad, 'Actividad Publicada'); // Notificar al NotificationCenter
+            }
+
+            // VERIFICAR PARA HACER EL RECORDATORIO
+            for (let fechaRecordatorio of fechasRecordatorio) {
+                const fechaRecordatorioTime = new Date(fechaRecordatorio).getTime();
+                const yaEnviado = recordatoriosEnviados.some(fecha => new Date(fecha).getTime() === fechaRecordatorioTime);
+
+                if (estadoActividad === 'Notificada' && ahora >= fechaRecordatorio && !yaEnviado) {
+                    actividad.recordatoriosEnviados.push(fechaRecordatorio);
+                    await actividad.save();
+                    await enviarNotificacion(actividad, 'Recordatorio de Actividad');
+                    //NotificationCenter.notify(actividad, 'Recordatorio de Actividad'); // Notificar al NotificationCenter
+                }
+            }
+            //actividad.accept(publishVisitor); // Publicar actividad
+            //actividad.accept(reminderVisitor); // Enviar recordatorio
         }
-      }
+    } catch (error) {
+        console.error('Error actualizando actividades:', error);
     }
-  } catch (error) {
-    console.error('Error actualizando actividades:', error);
-  }
 };
 
 module.exports = { actualizarEstadoActividades, setFechaSimulada };
--- server.js
+++ server.js
@@ -1,30 +1,29 @@
+// server.js
+
 require('dotenv').config();
 const express = require('express');
 const mongoose = require('mongoose');
 const path = require('path');
 const cron = require('node-cron');
 const bodyParser = require('body-parser');
+const { actualizarEstadoActividades, setFechaSimulada } = require('./actualizarActividades');
+const { PublishVisitor, ReminderVisitor } = require('./visitor');
 const personaRoutes = require('./routes/personaRoutes');
-const planesTrabajoRoutes= require('./routes/planesTrabajoRoutes');
-const actividadesRoutes= require('./routes/actividadesRoutes');
-const equiposTrabajoRoutes =  require('./routes/equiposTrabajoRoutes');
+const planesTrabajoRoutes = require('./routes/planesTrabajoRoutes');
+const actividadesRoutes = require('./routes/actividadesRoutes');
+const equiposTrabajoRoutes = require('./routes/equiposTrabajoRoutes');
 const estudianteRoutes = require('./routes/estudianteRoutes');
 const notificacionesRoutes = require('./routes/notificacionRoutes');
 const fechaSistemaRoutes = require('./routes/fechaSistemaRoutes');
 
-const { actualizarEstadoActividades, setFechaSimulada } = require('./actualizarActividades');
-
 const app = express();
 app.use(bodyParser.json());
 
-// Configurar node-cron para ejecutar la función cada minuto (ajusta según sea necesario)
-cron.schedule('*/10 * * * * *', () => {
+cron.schedule('*/20 * * * * *', () => {
   console.log('Ejecutando tarea de actualización de actividades');
   setFechaSimulada();
   actualizarEstadoActividades();
-  
 });
-
 
 app.use(express.json());
 
@@ -33,11 +32,9 @@ app.use((req, res, next) => {
   next();
 });
 
-//Guardar archivos estaticos
+
 app.use('/imgEstudiantes', express.static(path.join(__dirname, '../frontend/src/imgEstudiantes')));
 
-
-// Usa las rutas de personaRoutes
 app.use('/api/personaRoutes', personaRoutes);
 app.use('/api/estudianteRoutes', estudianteRoutes);
 app.use('/api/planesTrabajoRoutes', planesTrabajoRoutes);
@@ -45,7 +42,6 @@ app.use('/api/actividadesRoutes', actividadesRoutes);
 app.use('/api/equiposTrabajoRoutes', equiposTrabajoRoutes);
 app.use('/api/notificacionRoutes', notificacionesRoutes);
 app.use('/api/fechaSistemaRoutes', fechaSistemaRoutes);
-
 
 mongoose.connect(process.env.MONG_URI)
   .then(() => {
